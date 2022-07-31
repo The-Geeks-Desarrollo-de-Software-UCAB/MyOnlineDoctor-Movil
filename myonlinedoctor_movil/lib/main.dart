e5a2ebba438ffe1b98c87f4eaa60e445c15527d4
LAPTOP-4J8K1UJL\Vicente <vicentemirabal16@gmail.com>
2022-07-31 13:58:42 -0400

    Vista Aceptar o rechazar cita
    Se agrego la vista de aceptar o rechazar cita

--- myonlinedoctor_movil/lib/main.dart
+++ myonlinedoctor_movil/lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:myonlinedoctor_movil/dominio/cita.dart';
 import 'package:myonlinedoctor_movil/dominio/doctor.dart';
+import 'package:myonlinedoctor_movil/dominio/paciente.dart';
 import 'package:myonlinedoctor_movil/locator.dart';
+import 'package:myonlinedoctor_movil/presentacion/pages/detallecitaSolicitada.dart';
 import 'package:myonlinedoctor_movil/presentacion/pages/detalles_doctor.dart';
 import 'package:myonlinedoctor_movil/presentacion/pages/doctores_page.dart';
 import 'package:myonlinedoctor_movil/presentacion/pages/homePage.dart';
 
+import 'dominio/especialidades.dart';
 import 'presentacion/pages/loginPage.dart';
 import 'presentacion/pages/ratingPage.dart';
 import 'presentacion/pages/splashPage.dart';
@@ -26,6 +30,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: SplashPage(duration: 3, goToPage: HomePage()),
+      
     );
   }
 }
